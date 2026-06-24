a8b03899b97ad964ce43d0adc148c48f0e85804e
shereen <shereenelshahhat@gmail.com>
2026-06-25 02:30:03 +0300

    fix seed admin bug

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -7,7 +7,7 @@ export const dbconnect = async()=>{
     try{
         await mongoose.connect(process.env.DB_URI)
         console.log("db connected successfully")
-        seedAdmin()
+        await seedAdmin()
 
     }catch (error){
         console.error("Database connection failed:", error)
@@ -15,19 +15,22 @@ export const dbconnect = async()=>{
     
 }
 
-const seedAdmin = catchError(async(req,res,next)=>{
-    let admin = await User.findOne({role:"admin"})
+const seedAdmin = async () => {
+    let admin = await User.findOne({ role: "admin" });
+    
+    console.log("admin =", admin);
+    console.log("ADMIN_EMAIL =", process.env.ADMIN_EMAIL);
+    console.log("ADMIN_PASSWORD =", process.env.ADMIN_PASSWORD);
 
-    if(!admin){
+    if (!admin) {
         await User.create({
-            name:process.env.ADMIN_NAME,
-            email:process.env.ADMIN_EMAIL,
-            password:process.env.ADMIN_PASSWORD,
-            role:"admin",
-            age:25,
-        })
-        console.log("Admin Done")
-        console.log("ADMIN_EMAIL =", process.env.ADMIN_EMAIL);
-        console.log("ADMIN_PASSWORD =", process.env.ADMIN_PASSWORD);
+            name: process.env.ADMIN_NAME,
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD,
+            role: "admin",
+            age: 25,
+        });
+
+        console.log("Admin Done");
     }
-})
+};
