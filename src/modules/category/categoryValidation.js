9081ae0391ee7c627016b3fadee450fb1fe4e044
shereen <shereenelshahhat@gmail.com>
2026-06-25 03:21:11 +0300

    fixeg category

--- src/modules/category/categoryValidation.js
+++ src/modules/category/categoryValidation.js
@@ -9,6 +9,7 @@ export const addCategoryValiditor = Joi.object({
 export const updateCategoryValiditor = Joi.object({
     name:Joi.string().min(3).max(50).required(),
     id:Joi.string().hex().length(24).required(),
+    description:Joi.string().min(10).max(30).required(),
 })
 
 export const deleteCategoryValiditor = Joi.object({
